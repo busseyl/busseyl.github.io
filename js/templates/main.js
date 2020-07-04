be6ae6299b02c34847276cc4c403698abb60ee27
Lucas Bussey <busseyl@users.noreply.github.com>
2020-07-04 09:04:09 -0700

    Update main.js

--- js/templates/main.js
+++ js/templates/main.js
@@ -1,7 +1,5 @@
-<script type="text/x-template" id="main"> 
-  <v-app>
-    <v-main>
-      <v-container>Team of Monkeys Consulting</v-container>
-    </v-main>
-  </v-app>
-</script>
+<v-app>
+  <v-main>
+    <v-container>Team of Monkeys Consulting</v-container>
+  </v-main>
+</v-app>
