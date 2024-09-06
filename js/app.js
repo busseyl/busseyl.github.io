e331df7467d58261fd53589acfd3b95777089683
Lucas Bussey <busseyl@users.noreply.github.com>
2024-09-06 08:53:10 -0700

    Added Appwrite backend

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 /**
+ * Appwrite
+ */
+const client = new Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66db23330023a284ff78')
+
+/**
  * Main application
  */
 var vm = new Vue({
   el: '#app',
   template: '#main',
   vuetify: new Vuetify(),
+  data () {
+    return {
+      client,
+    }
+  }
 })
